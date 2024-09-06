9478ea7999aaf60814d40476ed05ac8e3f5abbfc
Benjamin Geißler <me@web-ben.de>
2024-09-06 22:20:52 +0200

    progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,24 @@
-import { Component, type OnInit, inject } from "@angular/core";
-import { provideNativeDateAdapter } from "@angular/material/core";
-import { RouterModule } from "@angular/router";
-import { AuthComponent } from "./auth.component";
-import { SupabaseService } from "./supabase.service";
+import { Component, type OnInit, inject } from '@angular/core';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { RouterModule } from '@angular/router';
+import { AuthComponent } from './auth.component';
+import { SupabaseService } from './supabase.service';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
-	standalone: true,
-	providers: [provideNativeDateAdapter()],
-	imports: [RouterModule, AuthComponent],
-	selector: "app-root",
-	templateUrl: "./app.component.html",
-	styleUrl: "./app.component.scss",
+  standalone: true,
+  providers: [provideNativeDateAdapter()],
+  imports: [RouterModule, AuthComponent, MatIconModule],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
 })
 export class AppComponent implements OnInit {
-	supabase = inject(SupabaseService);
+  supabase = inject(SupabaseService);
 
-	session = this.supabase.session;
+  session = this.supabase.session;
 
-	ngOnInit() {
-		this.supabase.authChanges((_, session) => (this.session = session));
-	}
+  ngOnInit() {
+    this.supabase.authChanges((_, session) => (this.session = session));
+  }
 }
--- src/components/gameview.component.ts
+++ src/components/gameview.component.ts
@@ -1,53 +1,69 @@
-import { CommonModule } from "@angular/common";
-import { Component, inject } from "@angular/core";
-import { SupabaseService } from "../app/supabase.service";
+import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
+import { ActionDTO, SupabaseService } from '../app/supabase.service';
+import { MatFormField } from '@angular/material/form-field';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
-	selector: "app-gameview",
-	imports: [CommonModule],
-	standalone: true,
-	template: `
-  <h1>Gameview</h1>
-  <ul class="flex flex-col">
-    @for (item of actions|async; track $index) {
+  selector: 'app-gameview',
+  imports: [
+    CommonModule,
+    MatFormField,
+    FormsModule,
+    ReactiveFormsModule,
+    MatSelectModule,
+  ],
+  standalone: true,
+  template: `
+    <div class="flex p-5">
+      <form [formGroup]="gameF">
+        <mat-form-field>
+          <mat-label>Game:</mat-label>
+          <mat-select formControlName="game_id">
+            @for (item of events|async; track $index) {
+            <mat-option [value]="item.id" selected>
+              {{ item.title }}
+            </mat-option>
+            }
+          </mat-select>
+        </mat-form-field>
+      </form>
+    </div>
+    <hr />
+    <ul class="flex flex-col">
+      @for (item of actions|async; track $index) {
       <li class="flex gap-2 text-pretty h-4 w-full justify-between ">
-        <span>{{item.player_id?.name}}</span>
-        <span>{{item.kind}}</span>
-        <span>{{item.character}}</span>
-        <span>{{item.grade}}</span>
-        <span>{{item.game_id?.title}}</span>
-        <span>{{item.game_id?.date}}</span>
-
+        <span>{{ item.player_id?.name }}</span>
+        <span>{{ item.kind }}</span>
+        <span>{{ item.character }}</span>
+        <span>{{ item.grade }}</span>
+        <span>{{ item.game_id?.title }}</span>
+        <span>{{ item.game_id?.date }}</span>
       </li>
-    }
-  </ul>
+      }
+    </ul>
   `,
 })
-export class GameViewComponent {
-	supabase = inject(SupabaseService);
-	actions = this.supabase.getActions() as Promise<ActionDTO[]>;
-}
+export class GameViewComponent implements OnInit {
+  supabase = inject(SupabaseService);
 
-export interface ActionDTO {
-	id: number;
-	created_at: Date;
-	kind: string;
-	character: string;
-	grade: string;
-	game_id: GameID;
-	player_id: PlayerID;
-}
+  events = this.supabase.getEvents();
 
-export interface GameID {
-	id: string;
-	date: Date;
-	title: string;
-	created_at: Date;
-}
-
-export interface PlayerID {
-	id: string;
-	name: string;
-	trikot: number;
-	created_at: Date;
+  actions?: Promise<ActionDTO[]>;
+  gameF = new FormGroup({
+    game_id: new FormControl<string>(''),
+  });
+  async ngOnInit() {
+    this.events = this.supabase.getEvents();
+    this.gameF.controls.game_id.setValue((await this.events)[0].id);
+    this.gameF.controls.game_id.valueChanges.subscribe((d) => {
+      if (d) this.actions = this.supabase.getActionsOfEvent(d);
+    });
+  }
 }
