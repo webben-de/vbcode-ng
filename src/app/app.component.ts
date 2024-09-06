93ef50e69bc566fbb27266946c13f730ac85f646
Geissler, Benjamin  SF/HZA-ZC3I <geissbnj@schaeffler.com>
2024-09-06 10:23:20 +0200

    feat: fetch real events, actions, player

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,138 +1,139 @@
-import { CommonModule } from '@angular/common';
-import { Component, inject, ViewChild, OnInit } from '@angular/core';
+import { CommonModule } from "@angular/common";
+import { Component, type OnInit, ViewChild, inject } from "@angular/core";
 import {
-  FormControl,
-  FormGroup,
-  FormsModule,
-  ReactiveFormsModule,
-  Validators,
-} from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
-import { MatButtonToggleModule } from '@angular/material/button-toggle';
-import { provideNativeDateAdapter } from '@angular/material/core';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatRadioModule } from '@angular/material/radio';
-import { type MatStepper, MatStepperModule } from '@angular/material/stepper';
-import { RouterModule } from '@angular/router';
-import { AuthComponent } from './auth.component';
-import { SupabaseService } from './supabase.service';
-import hints from './hints';
+	FormControl,
+	FormGroup,
+	FormsModule,
+	ReactiveFormsModule,
+	Validators,
+} from "@angular/forms";
+import { MatButtonModule } from "@angular/material/button";
+import { MatButtonToggleModule } from "@angular/material/button-toggle";
+import { provideNativeDateAdapter } from "@angular/material/core";
+import { MatDatepickerModule } from "@angular/material/datepicker";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { MatRadioModule } from "@angular/material/radio";
+import { MatSelectModule } from "@angular/material/select";
+import { type MatStepper, MatStepperModule } from "@angular/material/stepper";
+import { RouterModule } from "@angular/router";
+import { AuthComponent } from "./auth.component";
+import hints from "./hints";
+import { SupabaseService } from "./supabase.service";
+
 type PlTyp = {
-  nb: number;
-  name: string;
+	id: string;
+	trikot: number;
+	name: string;
 };
 type abbMap = {
-  abbr: string;
-  name: string;
+	abbr: string;
+	name: string;
 };
 
 @Component({
-  standalone: true,
-  providers: [provideNativeDateAdapter()],
-  imports: [
-    RouterModule,
-    FormsModule,
-    ReactiveFormsModule,
-    MatFormFieldModule,
-    MatButtonToggleModule,
-    MatButtonModule,
-    MatDatepickerModule,
-    MatInputModule,
-    MatRadioModule,
-    CommonModule,
-    MatStepperModule,
-    AuthComponent,
-  ],
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+	standalone: true,
+	providers: [provideNativeDateAdapter()],
+	imports: [
+		RouterModule,
+		FormsModule,
+		ReactiveFormsModule,
+		MatFormFieldModule,
+		MatButtonToggleModule,
+		MatButtonModule,
+		MatDatepickerModule,
+		MatInputModule,
+		MatRadioModule,
+		MatSelectModule,
+		CommonModule,
+		MatStepperModule,
+		AuthComponent,
+	],
+	selector: "app-root",
+	templateUrl: "./app.component.html",
+	styleUrl: "./app.component.scss",
 })
 export class AppComponent implements OnInit {
-  supabase = inject(SupabaseService);
-  actions = this.supabase.getActions();
-  title = 'vbcode-ng';
-  codeInFG = new FormGroup({
-    pl_date: new FormControl<Date>(new Date(), Validators.required),
-    ga_id: new FormControl<string>('', Validators.required),
-    pl_number: new FormControl<PlTyp | null>(null, Validators.required),
-    kind: new FormControl<abbMap | null>(null, Validators.required),
-    char: new FormControl<abbMap | null>(null, Validators.required),
-    grade: new FormControl<abbMap | null>(null, Validators.required),
-  });
+	supabase = inject(SupabaseService);
+	actions = this.supabase.getActions();
+	title = "vbcode-ng";
+	codeInFG = new FormGroup({
+		pl_date: new FormControl<Date>(new Date(), Validators.required),
+		game_id: new FormControl<string>("", Validators.required),
+		player_id: new FormControl<PlTyp | null>(null, Validators.required),
+		kind: new FormControl<abbMap | null>(null, Validators.required),
+		char: new FormControl<abbMap | null>(null, Validators.required),
+		grade: new FormControl<abbMap | null>(null, Validators.required),
+	});
 
-  hint_texts: {
-    [kind: string]: {
-      [grade: string]: string;
-    };
-  } = hints;
-  pl: PlTyp[] = [
-    { nb: 10, name: 'Ben' },
-    { nb: 29, name: 'Ka' },
-    { nb: 15, name: 'Tu' },
-    { nb: 22, name: 'Ma' },
-    { nb: 77, name: 'Ba' },
-    { nb: 17, name: 'An' },
-    { nb: 42, name: 'To' },
-    { nb: 14, name: 'Mi' },
-    { nb: 7, name: 'Jo' },
-    { nb: 4, name: 'Be' },
-  ].sort((a, b) => a.nb - b.nb);
-  /**
-   *
-   */
-  kind_options: abbMap[] = [
-    { abbr: 'S', name: 'Serve' },
-    { abbr: 'R', name: 'Recieve' },
-    { abbr: 'A', name: 'A' },
-    { abbr: 'B', name: 'Block' },
-    { abbr: 'D', name: 'Def' },
-    { abbr: 'E', name: 'Set' },
-    { abbr: 'F', name: 'Free' },
-  ];
-  /**
+	hint_texts: {
+		[kind: string]: {
+			[grade: string]: string;
+		};
+	} = hints;
+	player = this.supabase.getPlayers();
+	events = this.supabase.getEvents();
+	/**
+	 *
+	 */
+	kind_options: abbMap[] = [
+		{ abbr: "S", name: "Serve" },
+		{ abbr: "R", name: "Recieve" },
+		{ abbr: "A", name: "A" },
+		{ abbr: "B", name: "Block" },
+		{ abbr: "D", name: "Def" },
+		{ abbr: "E", name: "Set" },
+		{ abbr: "F", name: "Free" },
+	];
+	/**
    * 
         
    */
-  char_options: abbMap[] = [
-    { abbr: 'H', name: 'High' },
-    { abbr: 'M', name: 'Medium' },
-    { abbr: 'Q', name: 'Quick' },
-    { abbr: 'T', name: 'Tense' },
-    { abbr: 'U', name: 'Super' },
-    { abbr: 'F', name: 'Fast' },
-    { abbr: 'O', name: 'Other' },
-  ];
-  /**
-   *
-   */
-  grad_options: abbMap[] = [
-    { abbr: '#', name: '#' },
-    { abbr: '+', name: '+' },
-    { abbr: '!', name: '!' },
-    { abbr: '/', name: '/' },
-    { abbr: '-', name: '-' },
-    { abbr: '=', name: '=' },
-  ];
-  @ViewChild('stepper') stepper!: MatStepper;
-  /**
-   *
-   */
-  async submit() {
-    const { pl_number, pl_date } = this.codeInFG.value;
-    try {
-      const a = await this.supabase.createAction(this.codeInFG.value);
-      console.info(a);
-      this.codeInFG.reset({ pl_date });
-      this.stepper.reset();
-    } catch (error) {
-      console.error(error);
-    }
-  }
-  session = this.supabase.session;
+	char_options: abbMap[] = [
+		{ abbr: "H", name: "High" },
+		{ abbr: "M", name: "Medium" },
+		{ abbr: "Q", name: "Quick" },
+		{ abbr: "T", name: "Tense" },
+		{ abbr: "U", name: "Super" },
+		{ abbr: "F", name: "Fast" },
+		{ abbr: "O", name: "Other" },
+	];
+	/**
+	 *
+	 */
+	grad_options: abbMap[] = [
+		{ abbr: "#", name: "#" },
+		{ abbr: "+", name: "+" },
+		{ abbr: "!", name: "!" },
+		{ abbr: "/", name: "/" },
+		{ abbr: "-", name: "-" },
+		{ abbr: "=", name: "=" },
+	];
+	@ViewChild("stepper") stepper!: MatStepper;
+	/**
+	 *
+	 */
+	async submit() {
+		const { pl_date, game_id } = this.codeInFG.value;
+		const payload = {
+			player_id: this.codeInFG.controls.player_id.value?.id,
+			kind: this.codeInFG.controls.kind.value?.abbr,
+			character: this.codeInFG.controls.char.value?.abbr,
+			grade: this.codeInFG.controls.grade.value?.abbr,
+			game_id: this.codeInFG.controls.game_id.value,
+		};
+		try {
+			const a = await this.supabase.createAction(payload);
+			this.actions = this.supabase.getActions();
+			this.codeInFG.reset({ pl_date, game_id });
+			this.stepper.reset();
+		} catch (error) {
+			console.error(error);
+		}
+	}
+	session = this.supabase.session;
 
-  ngOnInit() {
-    this.supabase.authChanges((_, session) => (this.session = session));
-  }
+	ngOnInit() {
+		this.supabase.authChanges((_, session) => (this.session = session));
+	}
 }
--- src/app/supabase.service.ts
+++ src/app/supabase.service.ts
@@ -1,99 +1,97 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 import {
-  AuthChangeEvent,
-  AuthSession,
-  createClient,
-  Session,
-  SupabaseClient,
-  User,
-} from '@supabase/supabase-js';
-import { environment } from '../environments/environment';
+	type AuthChangeEvent,
+	type AuthSession,
+	type Session,
+	type SupabaseClient,
+	type User,
+	createClient,
+} from "@supabase/supabase-js";
+import { environment } from "../environments/environment";
 
 export interface Profile {
-  id?: string;
-  username: string;
-  website: string;
-  avatar_url: string;
+	id?: string;
+	username: string;
+	website: string;
+	avatar_url: string;
 }
 
 @Injectable({
-  providedIn: 'root',
+	providedIn: "root",
 })
 export class SupabaseService {
-  createAction(
-    value: Partial<{
-      pl_date: Date | null;
-      ga_id: string | null;
-      pl_number: { nb: number; name: string } | null;
-      kind: { abbr: string; name: string } | null;
-      char: { abbr: string; name: string } | null;
-      grade: { abbr: string; name: string } | null;
-    }>
-  ) {
-    this.supabase
-      .from('actions')
-      .upsert({ player_id: value?.pl_number?.nb })
-      .select();
-  }
-  private supabase: SupabaseClient;
-  _session: AuthSession | null = null;
+	private supabase: SupabaseClient;
+	_session: AuthSession | null = null;
 
-  constructor() {
-    this.supabase = createClient(
-      environment.supabaseUrl,
-      environment.supabaseKey
-    );
-  }
+	constructor() {
+		this.supabase = createClient(
+			environment.supabaseUrl,
+			environment.supabaseKey,
+		);
+	}
 
-  get session() {
-    this.supabase.auth.getSession().then(({ data }) => {
-      this._session = data.session;
-    });
-    return this._session;
-  }
+	get session() {
+		this.supabase.auth.getSession().then(({ data }) => {
+			this._session = data.session;
+		});
+		return this._session;
+	}
 
-  profile(user: User) {
-    return this.supabase
-      .from('profiles')
-      .select(`username, website, avatar_url`)
-      .eq('id', user.id)
-      .single();
-  }
+	profile(user: User) {
+		return this.supabase
+			.from("profiles")
+			.select("username, website, avatar_url")
+			.eq("id", user.id)
+			.single();
+	}
 
-  authChanges(
-    callback: (event: AuthChangeEvent, session: Session | null) => void
-  ) {
-    return this.supabase.auth.onAuthStateChange(callback);
-  }
+	authChanges(
+		callback: (event: AuthChangeEvent, session: Session | null) => void,
+	) {
+		return this.supabase.auth.onAuthStateChange(callback);
+	}
 
-  signInOTP(email: string) {
-    return this.supabase.auth.signInWithOtp({ email });
-  }
-  signInPW(email: string, password: string) {
-    return this.supabase.auth.signInWithPassword({ email, password });
-  }
+	signInOTP(email: string) {
+		return this.supabase.auth.signInWithOtp({ email });
+	}
+	signInPW(email: string, password: string) {
+		return this.supabase.auth.signInWithPassword({ email, password });
+	}
 
-  signOut() {
-    return this.supabase.auth.signOut();
-  }
+	signOut() {
+		return this.supabase.auth.signOut();
+	}
 
-  updateProfile(profile: Profile) {
-    const update = {
-      ...profile,
-      updated_at: new Date(),
-    };
+	updateProfile(profile: Profile) {
+		const update = {
+			...profile,
+			updated_at: new Date(),
+		};
 
-    return this.supabase.from('profiles').upsert(update);
-  }
-  getActions() {
-    return this.supabase.from('actions').select('*');
-  }
+		return this.supabase.from("profiles").upsert(update);
+	}
+	async getEvents() {
+		return (await this.supabase.from("events").select("*")).data;
+	}
+	async getPlayers() {
+		return (await this.supabase.from("players").select("*")).data as {
+			id: string;
+			name: string;
+			trikot: number;
+		}[];
+	}
+	async getActions() {
+		return (await this.supabase.from("actions").select("*")).data;
+	}
+	async createAction(value: any) {
+		return (await this.supabase.from("actions").upsert(value).select()).data;
+	}
 
-  downLoadImage(path: string) {
-    return this.supabase.storage.from('avatars').download(path);
-  }
+	downLoadImage(path: string) {
+		return this.supabase.storage.from("avatars").download(path);
+	}
 
-  uploadAvatar(filePath: string, file: File) {
-    return this.supabase.storage.from('avatars').upload(filePath, file);
-  }
+	uploadAvatar(filePath: string, file: File) {
+		return this.supabase.storage.from("avatars").upload(filePath, file);
+	}
 }
