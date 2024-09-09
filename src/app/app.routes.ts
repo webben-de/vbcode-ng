44b36107799243f8026c7b96d9f9ba5014a610d4
Geissler, Benjamin  SF/HZA-ZC3I <geissbnj@schaeffler.com>
2024-09-09 15:44:55 +0200

    feat: add details page with working charts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,35 @@
-import type { Route } from '@angular/router';
+import { inject } from '@angular/core';
+import type { ActivatedRouteSnapshot, Route } from '@angular/router';
+import { DataEntryComponent } from '../components/data-entry.component';
+import { GameDetailViewComponent } from '../components/game-detail-view.component';
 import { GameViewComponent } from '../components/gameview.component';
 import { AppComponent } from './app.component';
-import { DataEntryComponent } from '../components/data-entry.component';
+import { SupabaseService } from './supabase.service';
 
 export const appRoutes: Route[] = [
-  { path: 'gameview', component: GameViewComponent },
+  {
+    path: 'gameview',
+    component: GameViewComponent,
+  },
+  {
+    path: 'gameview/:id',
+    component: GameDetailViewComponent,
+    resolve: {
+      actions: actionResolver(),
+      game: (route: ActivatedRouteSnapshot) => {
+        // biome-ignore lint/style/noNonNullAssertion: <explanation>
+        return inject(SupabaseService).getEvent(route.paramMap.get('id')!);
+      },
+    },
+  },
   { path: 'dataentry', component: DataEntryComponent },
   { path: '*', component: AppComponent },
 ];
+function actionResolver() {
+  return (route: ActivatedRouteSnapshot) => {
+    return inject(SupabaseService).getActionsOfEvent(
+      // biome-ignore lint/style/noNonNullAssertion: <explanation>
+      route.paramMap.get('id')!
+    );
+  };
+}
