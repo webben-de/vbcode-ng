e41b1871252b5e433b73c0604e17e2bbdab593fe
Geissler, Benjamin  SF/HZA-ZC3I <geissbnj@schaeffler.com>
2024-09-10 11:28:38 +0200

    feat: generic kinds

--- src/components/game-detail-view.component.ts
+++ src/components/game-detail-view.component.ts
@@ -1,10 +1,8 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { CommonModule } from '@angular/common';
-import { Component, type OnInit, computed, inject } from '@angular/core';
+import { Component, type OnInit, inject } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import {
-  type MatSelectChange,
-  MatSelectModule,
-} from '@angular/material/select';
+import { MatSelectModule } from '@angular/material/select';
 import { ActivatedRoute } from '@angular/router';
 import type { EChartsOption } from 'echarts';
 import { NgxEchartsDirective } from 'ngx-echarts';
@@ -12,9 +10,111 @@ import type { ActionDTO } from 'src/types/ActionDTO';
 import { ActionsService } from '../services/action.service';
 import type { EventDTO } from '../services/events.service';
 import { SupabaseService } from '../services/supabase.service';
-import { type ActionGrade, grad_options_list } from './grade-options';
-import { ActionKind } from './kind-options';
+import { ActionGrade, grad_options_list } from './grade-options';
+import { ActionKind, kinds } from './kind-options';
 
+const defaults:
+  | {
+      [x in ActionKind]: {
+        title?: string;
+        total: number;
+        most_player: { [x: string]: number };
+        stats: {
+          [x in ActionGrade]: number;
+        };
+        charts?: {
+          gradePie: EChartsOption;
+          playerPie: EChartsOption;
+        };
+      };
+    } = {
+  [ActionKind.Serve]: {
+    title: 'Serve',
+    total: 0,
+    most_player: {},
+    stats: {
+      [ActionGrade['#']]: 0,
+      [ActionGrade['+']]: 0,
+      [ActionGrade['!']]: 0,
+      [ActionGrade['/']]: 0,
+      [ActionGrade['-']]: 0,
+      [ActionGrade['=']]: 0,
+    },
+  },
+  [ActionKind.Recieve]: {
+    title: 'Recieve',
+    total: 0,
+    most_player: {},
+    stats: {
+      [ActionGrade['#']]: 0,
+      [ActionGrade['+']]: 0,
+      [ActionGrade['!']]: 0,
+      [ActionGrade['/']]: 0,
+      [ActionGrade['-']]: 0,
+      [ActionGrade['=']]: 0,
+    },
+  },
+  [ActionKind.Attack]: {
+    total: 0,
+    most_player: {},
+    stats: {
+      [ActionGrade['#']]: 0,
+      [ActionGrade['+']]: 0,
+      [ActionGrade['!']]: 0,
+      [ActionGrade['/']]: 0,
+      [ActionGrade['-']]: 0,
+      [ActionGrade['=']]: 0,
+    },
+  },
+  [ActionKind.Block]: {
+    total: 0,
+    most_player: {},
+    stats: {
+      [ActionGrade['#']]: 0,
+      [ActionGrade['+']]: 0,
+      [ActionGrade['!']]: 0,
+      [ActionGrade['/']]: 0,
+      [ActionGrade['-']]: 0,
+      [ActionGrade['=']]: 0,
+    },
+  },
+  [ActionKind.Def]: {
+    total: 0,
+    most_player: {},
+    stats: {
+      [ActionGrade['#']]: 0,
+      [ActionGrade['+']]: 0,
+      [ActionGrade['!']]: 0,
+      [ActionGrade['/']]: 0,
+      [ActionGrade['-']]: 0,
+      [ActionGrade['=']]: 0,
+    },
+  },
+  [ActionKind.Set]: {
+    total: 0,
+    most_player: {},
+    stats: {
+      [ActionGrade['#']]: 0,
+      [ActionGrade['+']]: 0,
+      [ActionGrade['!']]: 0,
+      [ActionGrade['/']]: 0,
+      [ActionGrade['-']]: 0,
+      [ActionGrade['=']]: 0,
+    },
+  },
+  [ActionKind.Free]: {
+    total: 0,
+    most_player: {},
+    stats: {
+      [ActionGrade['#']]: 0,
+      [ActionGrade['+']]: 0,
+      [ActionGrade['!']]: 0,
+      [ActionGrade['/']]: 0,
+      [ActionGrade['-']]: 0,
+      [ActionGrade['=']]: 0,
+    },
+  },
+};
 @Component({
   selector: 'app-game-game-detail-view',
   standalone: true,
@@ -43,16 +143,19 @@ import { ActionKind } from './kind-options';
         </mat-select>
       </mat-form-field>
       <section class="flex flex-col gap-4">
+        @for (stat of stats|keyvalue; track $index) {
         <div class="collapse collapse-arrow bg-base-200">
           <input type="radio" name="acc-attacks" checked="checked" />
-          <div class="collapse-title text-xl font-medium">Attacks</div>
+          <div class="collapse-title text-xl font-medium">{{ stat.key }}</div>
           <div class="collapse-content">
             <section class="flex flex-col gap-2">
               <div class="stats shadow">
                 <div class="stat">
                   <div class="stat-figure text-primary"></div>
-                  <div class="stat-title">Total Attacks</div>
-                  <div class="stat-value text-primary">{{ attacks.total }}</div>
+                  <div class="stat-title">Total {{ stat.key }}</div>
+                  <div class="stat-value text-primary">
+                    {{ stat.value.total }}
+                  </div>
                 </div>
               </div>
               <h4>Nach Bewertung</h4>
@@ -61,41 +164,46 @@ import { ActionKind } from './kind-options';
                 <div class="stat">
                   <div class="stat-title">{{ item }}</div>
                   <div class="stat-value text-primary">
-                    {{ attacks.stats[item] }}
+                    {{ stat.value.stats[item] }}
                   </div>
-                  @if (attacks.stats[item]) {
+                  @if (stat.value.stats[item]) {
                   <div class="stat-desc text-primary">
-                    {{ attacks.stats[item] / attacks.total | percent }}
+                    {{ stat.value.stats[item] / stat.value.total | percent }}
                   </div>
                   }
                 </div>
                 }
               </div>
+              @if (stat.value.charts?.gradePie; as pie) {
               <div
                 echarts
-                [options]="gradePie"
+                [options]="pie"
                 [initOpts]="{ renderer: 'canvas' }"
                 class="h-40 w-full"
               ></div>
+              }
               <h4>Nach Spieler</h4>
               <div class="stats shadow">
-                @for (item of attacks.most_player|keyvalue; track $index) {
+                @for (item of stat.value.most_player|keyvalue; track $index) {
                 <div class="stat">
                   <div class="stat-title">{{ item.key }}</div>
                   <div class="stat-value text-primary">{{ item.value }}</div>
                 </div>
                 }
               </div>
+              @if (stat.value.charts?.playerPie; as pie) {
               <div
                 echarts
-                [options]="playerPie"
+                [options]="pie"
                 [initOpts]="{ renderer: 'canvas' }"
                 class="h-40 w-full"
               ></div>
+              }
               <hr />
             </section>
           </div>
         </div>
+        }
         <div class="collapse collapse-arrow bg-base-200">
           <input type="radio" name="acc-attacks" checked="checked" />
           <div class="collapse-title text-xl font-medium">Log</div>
@@ -137,10 +245,8 @@ import { ActionKind } from './kind-options';
 })
 export class GameDetailViewComponent implements OnInit {
   currentSet: string | number | undefined;
-  async deleteAction(arg0: number) {
-    const arg1 = arg0;
-    return await this.actionsService.deleteAction(arg0);
-  }
+  stats = defaults;
+
   activatedRoute = inject(ActivatedRoute);
   supabase = inject(SupabaseService);
   actionsService = inject(ActionsService);
@@ -181,10 +287,43 @@ export class GameDetailViewComponent implements OnInit {
     // const aces = await this.actionsService.getAcesOfEvent(game.id);
     // const stats = await this.supabase.getGameStats(game.id);
     this.actions = a;
-    this.aggregateAttacks(a);
-    this.updateCharts();
+
+    this.generateStats(a, ActionKind.Attack);
+    this.generateStats(a, ActionKind.Set);
+    this.generateStats(a, ActionKind.Block);
+    this.generateStats(a, ActionKind.Def);
+    this.generateStats(a, ActionKind.Free);
+    this.generateStats(a, ActionKind.Recieve);
+    this.generateStats(a, ActionKind.Serve);
   }
 
+  private generateStats(a: ActionDTO[], kind: ActionKind) {
+    if (!this.stats[kind]) return;
+    this.stats[kind].total = a.filter((a) => a.kind === kind).length;
+
+    // biome-ignore lint/complexity/noForEach: <explanation>
+    this.actions
+      .filter((a) => a.kind === kind)
+      .map((a) => a.player_id.name)
+      .forEach((player) => {
+        if (this.stats[kind].most_player[player])
+          this.stats[kind].most_player[player] += 1;
+        else this.stats[kind].most_player[player] = 1;
+      });
+    this.stats[kind].most_player = Object.fromEntries(
+      Object.entries(this.stats[kind].most_player).sort(
+        (a: any, b: any) => a[1] - b[1]
+      )
+    );
+
+    // biome-ignore lint/complexity/noForEach: <explanation>
+    grad_options_list.forEach(({ name }) => {
+      this.stats[kind].stats[name] = a.filter(
+        (a) => a.grade === name && a.kind === kind
+      ).length;
+    });
+    this.updateCharts(kind);
+  }
   private aggregateAttacks(a: ActionDTO[]) {
     this.attacks = { total: 0, most_player: {}, stats: {} };
 
@@ -211,23 +350,29 @@ export class GameDetailViewComponent implements OnInit {
     });
   }
 
-  private updateCharts() {
-    this.gradePie = { series: [] };
-    this.gradePie.series = [
+  private updateCharts(kind: ActionKind) {
+    if (!this.stats[kind].charts)
+      this.stats[kind].charts = {
+        gradePie: {},
+        playerPie: {},
+      };
+    this.stats[kind].charts.gradePie = { series: [] };
+    this.stats[kind].charts.gradePie.series = [
       {
         type: 'pie',
         radius: '50%',
-        data: Object.entries(this.attacks.stats).map((v) => ({
+        data: Object.entries(this.stats[kind].stats).map((v) => ({
           value: v[1],
           name: v[0],
         })),
       },
     ];
-    this.playerPie.series = [
+    this.stats[kind].charts.playerPie = { series: [] };
+    this.stats[kind].charts.playerPie.series = [
       {
         type: 'pie',
         radius: '50%',
-        data: Object.entries(this.attacks.most_player).map((v) => ({
+        data: Object.entries(this.stats[kind].most_player).map((v) => ({
           value: v[1],
           name: v[0],
         })),
@@ -249,5 +394,9 @@ export class GameDetailViewComponent implements OnInit {
         $event as number
       );
     this.updateStats(this.actions, this.game);
+  }
+  async deleteAction(arg0: number) {
+    const arg1 = arg0;
+    return await this.actionsService.deleteAction(arg0);
   }
 }
