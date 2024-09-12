ddece51e62501210bf55655268246f25ca87b348
Benjamin Geißler <me@web-ben.de>
2024-09-12 18:32:46 +0200

    feat: better auth access

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,24 +3,24 @@ import { provideNativeDateAdapter } from '@angular/material/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import { Router, RouterModule } from '@angular/router';
+import { Router, RouterEvent, RouterModule } from '@angular/router';
 import { initFlowbite } from 'flowbite';
 import { SupabaseService } from '../services/supabase.service';
 import { AuthComponent } from './auth.component';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+import { filter } from 'rxjs';
 
 @Component({
   standalone: true,
   providers: [provideNativeDateAdapter()],
-  imports: [RouterModule, AuthComponent, MatIconModule, MatSidenavModule, MatListModule],
+  imports: [RouterModule, AuthComponent, MatIconModule, MatSidenavModule, MatListModule, MatSnackBarModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
 export class AppComponent implements OnInit {
-  logout() {
-    this.supabase.signOut();
-  }
-  supabase = inject(SupabaseService);
+  snack = inject(MatSnackBar);
   router = inject(Router);
+  supabase = inject(SupabaseService);
 
   session = this.supabase.session;
 
@@ -29,6 +29,16 @@ export class AppComponent implements OnInit {
       this.session = session;
       // this.router.navigate(['/report']);
     });
+    // this.router.events.pipe(filter())
     initFlowbite();
+  }
+  async logout() {
+    try {
+      await this.supabase.signOut();
+      this.snack.open('Logged out', '', { duration: 1000 });
+      this.router.navigate(['/']);
+    } catch (error) {
+      this.snack.open('Error while logging out', '', { duration: 1000 });
+    }
   }
 }
