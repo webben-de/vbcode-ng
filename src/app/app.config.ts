4f1c6b2b556f14c215643662ec09d8884a85d570
Geissler, Benjamin  SF/HZA-ZC3I <geissbnj@schaeffler.com>
2024-09-17 08:53:26 +0200

    feat: pwa support

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { provideStore } from '@ngxs/store';
-import { type ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { type ApplicationConfig, provideZoneChangeDetection, isDevMode } from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { BarChart, PieChart, RadarChart } from 'echarts/charts';
@@ -8,6 +8,7 @@ import * as echarts from 'echarts/core';
 import { CanvasRenderer } from 'echarts/renderers';
 import { provideEchartsCore } from 'ngx-echarts';
 import { appRoutes } from './app.routes';
+import { provideServiceWorker } from '@angular/service-worker';
 
 echarts.use([CanvasRenderer, BarChart, PieChart, RadarChart, TitleComponent, LegendComponent]);
 
@@ -17,6 +18,9 @@ export const appConfig: ApplicationConfig = {
     provideRouter(appRoutes, withInMemoryScrolling()),
     provideAnimationsAsync(),
     provideEchartsCore({ echarts }),
-    provideStore(),
+    provideStore(), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          }),
   ],
 };
